09a449f1b1c726a83f87d6aa8ae05de206bdd998
SIRSoares33 <sirgustavo4@gmail.com>
2023-12-14 19:34:44 -0300

    style: Criando a tela principal do MainView

--- Calculadora/MVP/View/Main/MainView.Designer.cs
+++ Calculadora/MVP/View/Main/MainView.Designer.cs
@@ -28,11 +28,230 @@ partial class MainView
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        pnlInput = new Panel();
+        txtInput = new TextBox();
+        pnlButtons = new Panel();
+        button13 = new Button();
+        button12 = new Button();
+        button11 = new Button();
+        button10 = new Button();
+        button9 = new Button();
+        button8 = new Button();
+        button4 = new Button();
+        button7 = new Button();
+        button6 = new Button();
+        button5 = new Button();
+        button3 = new Button();
+        button2 = new Button();
+        button1 = new Button();
+        pnlInput.SuspendLayout();
+        pnlButtons.SuspendLayout();
+        SuspendLayout();
+        // 
+        // pnlInput
+        // 
+        pnlInput.BackColor = SystemColors.ActiveCaption;
+        pnlInput.Controls.Add(txtInput);
+        pnlInput.Location = new Point(0, 23);
+        pnlInput.Name = "pnlInput";
+        pnlInput.Size = new Size(324, 72);
+        pnlInput.TabIndex = 0;
+        // 
+        // txtInput
+        // 
+        txtInput.BackColor = SystemColors.ActiveCaption;
+        txtInput.BorderStyle = BorderStyle.None;
+        txtInput.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+        txtInput.Location = new Point(-22, 18);
+        txtInput.Name = "txtInput";
+        txtInput.ReadOnly = true;
+        txtInput.Size = new Size(324, 39);
+        txtInput.TabIndex = 0;
+        txtInput.TextAlign = HorizontalAlignment.Right;
+        // 
+        // pnlButtons
+        // 
+        pnlButtons.Controls.Add(button13);
+        pnlButtons.Controls.Add(button12);
+        pnlButtons.Controls.Add(button11);
+        pnlButtons.Controls.Add(button10);
+        pnlButtons.Controls.Add(button9);
+        pnlButtons.Controls.Add(button8);
+        pnlButtons.Controls.Add(button4);
+        pnlButtons.Controls.Add(button7);
+        pnlButtons.Controls.Add(button6);
+        pnlButtons.Controls.Add(button5);
+        pnlButtons.Controls.Add(button3);
+        pnlButtons.Controls.Add(button2);
+        pnlButtons.Controls.Add(button1);
+        pnlButtons.Location = new Point(0, 109);
+        pnlButtons.Name = "pnlButtons";
+        pnlButtons.Size = new Size(324, 368);
+        pnlButtons.TabIndex = 1;
+        // 
+        // button13
+        // 
+        button13.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+        button13.Location = new Point(0, 184);
+        button13.Name = "button13";
+        button13.Size = new Size(77, 69);
+        button13.TabIndex = 13;
+        button13.Text = "/";
+        button13.UseVisualStyleBackColor = true;
+        // 
+        // button12
+        // 
+        button12.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+        button12.Location = new Point(225, 120);
+        button12.Name = "button12";
+        button12.Size = new Size(77, 69);
+        button12.TabIndex = 12;
+        button12.Text = "*";
+        button12.UseVisualStyleBackColor = true;
+        // 
+        // button11
+        // 
+        button11.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+        button11.Location = new Point(149, 120);
+        button11.Name = "button11";
+        button11.Size = new Size(77, 69);
+        button11.TabIndex = 11;
+        button11.Text = "-";
+        button11.UseVisualStyleBackColor = true;
+        // 
+        // button10
+        // 
+        button10.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+        button10.Location = new Point(76, 120);
+        button10.Name = "button10";
+        button10.Size = new Size(77, 69);
+        button10.TabIndex = 10;
+        button10.Text = "+";
+        button10.UseVisualStyleBackColor = true;
+        // 
+        // button9
+        // 
+        button9.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+        button9.Location = new Point(0, 120);
+        button9.Name = "button9";
+        button9.Size = new Size(77, 69);
+        button9.TabIndex = 9;
+        button9.Text = "9";
+        button9.UseVisualStyleBackColor = true;
+        // 
+        // button8
+        // 
+        button8.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+        button8.Location = new Point(225, 58);
+        button8.Name = "button8";
+        button8.Size = new Size(77, 69);
+        button8.TabIndex = 8;
+        button8.Text = "8";
+        button8.UseVisualStyleBackColor = true;
+        // 
+        // button4
+        // 
+        button4.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+        button4.Location = new Point(225, -8);
+        button4.Name = "button4";
+        button4.Size = new Size(77, 69);
+        button4.TabIndex = 7;
+        button4.Text = "4";
+        button4.UseVisualStyleBackColor = true;
+        // 
+        // button7
+        // 
+        button7.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+        button7.Location = new Point(149, 58);
+        button7.Name = "button7";
+        button7.Size = new Size(77, 69);
+        button7.TabIndex = 6;
+        button7.Text = "7";
+        button7.UseVisualStyleBackColor = true;
+        // 
+        // button6
+        // 
+        button6.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+        button6.Location = new Point(76, 58);
+        button6.Name = "button6";
+        button6.Size = new Size(77, 69);
+        button6.TabIndex = 5;
+        button6.Text = "6";
+        button6.UseVisualStyleBackColor = true;
+        // 
+        // button5
+        // 
+        button5.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+        button5.Location = new Point(0, 58);
+        button5.Name = "button5";
+        button5.Size = new Size(77, 69);
+        button5.TabIndex = 4;
+        button5.Text = "5";
+        button5.UseVisualStyleBackColor = true;
+        // 
+        // button3
+        // 
+        button3.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+        button3.Location = new Point(149, -8);
+        button3.Name = "button3";
+        button3.Size = new Size(77, 69);
+        button3.TabIndex = 2;
+        button3.Text = "3";
+        button3.UseVisualStyleBackColor = true;
+        // 
+        // button2
+        // 
+        button2.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+        button2.Location = new Point(76, -8);
+        button2.Name = "button2";
+        button2.Size = new Size(75, 69);
+        button2.TabIndex = 1;
+        button2.Text = "2";
+        button2.UseVisualStyleBackColor = true;
+        // 
+        // button1
+        // 
+        button1.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+        button1.Location = new Point(0, -8);
+        button1.Name = "button1";
+        button1.Size = new Size(77, 69);
+        button1.TabIndex = 0;
+        button1.Text = "1";
+        button1.UseVisualStyleBackColor = true;
+        // 
+        // MainView
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        BackColor = SystemColors.ButtonShadow;
+        ClientSize = new Size(302, 360);
+        Controls.Add(pnlButtons);
+        Controls.Add(pnlInput);
+        FormBorderStyle = FormBorderStyle.None;
+        Name = "MainView";
+        Text = "Form1";
+        pnlInput.ResumeLayout(false);
+        pnlInput.PerformLayout();
+        pnlButtons.ResumeLayout(false);
+        ResumeLayout(false);
     }
 
     #endregion
+
+    private Panel pnlInput;
+    private TextBox txtInput;
+    private Panel pnlButtons;
+    private Button button1;
+    private Button button3;
+    private Button button2;
+    private Button button13;
+    private Button button12;
+    private Button button11;
+    private Button button10;
+    private Button button9;
+    private Button button8;
+    private Button button4;
+    private Button button7;
+    private Button button6;
+    private Button button5;
 }
